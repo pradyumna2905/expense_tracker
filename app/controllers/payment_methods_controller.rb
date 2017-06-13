5fb9b74191b7bbaf9578064b49b6a3d607efea63
Pradyumna <pradyumna2905@gmail.com>
2017-06-13 15:07:06 -0700

    Payment controller, routes, views.

---
+++ app/controllers/payment_methods_controller.rb
@@ -0,0 +1,21 @@
+class PaymentMethodsController < ApplicationController
+  def new
+    @payment_method = current_user.payment_methods.build
+  end
+
+  def create
+    @payment_method = current_user.payment_methods.build(create_params)
+
+    if @payment_method.save
+      redirect_to new_expense_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def create_params
+    params.require(:payment_method).permit(:name)
+  end
+end
