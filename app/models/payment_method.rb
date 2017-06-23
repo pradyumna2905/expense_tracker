e231f757033b849802c9166b498bea4b462f29f9
Pradyumna <pradyumna2905@gmail.com>
2017-06-23 00:17:39 -0700

    Refactor graphs

--- app/models/payment_method.rb
+++ app/models/payment_method.rb
@@ -1,6 +1,9 @@
 class PaymentMethod < ApplicationRecord
   DEFAULT_PAYMENT_METHOD = "Cash"
 
+  scope :weekly_expense_report, -> { group(:name).joins(:expenses).
+    where("expenses.date >= ?", 1.week.ago) }
+
   belongs_to :user
   has_many :expenses
 
