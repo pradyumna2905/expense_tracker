b4a4e4372530dbc8ae5f90235054f1a270864509
Pradyumna <pradyumna2905@gmail.com>
2017-06-22 23:05:19 -0700

    Some nav styles.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def formatted_date(date)
     date.strftime("%B %d, %Y")
   end
+
+  def sub_nav_link(text, path)
+    link_to text.to_s, path,
+      class: "sub-nav-link"
+  end
 end
