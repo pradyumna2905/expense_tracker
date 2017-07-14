5e841178c4582d027662e5ff037c3c563868b135
Pradyumna <pradyumna2905@gmail.com>
2017-07-13 20:51:28 -0700

    Switch to haml and some test modifications.

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -16,8 +16,8 @@ class ExpensesController < ApplicationController
   end
 
   def index
-    @expenses = current_user.expenses.
-      includes(:payment_method, :category).desc.page(params[:page])
+    @expenses = current_user.expenses.desc.
+      includes(:payment_method, :category).page(params[:page])
     @grand_total = current_user.grand_total
   end
 
